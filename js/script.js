a4e10e00b81c3e53ecdec7d4bedc9acec162b697
SmavaProject <katyakravchenka@yahoo.com>
2020-04-12 22:21:37 -0700

    costs and tshirt tasks

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const title = document.getElementById("title");
 const hiddenLabel = document.querySelector(".hidden-label");
 const hiddenInput = document.querySelector(".hidden-input");
 
+const desing = document.getElementById("design");
+
+const activities = document.querySelector(".activities");
+
 nameField.focus();
 
 title.addEventListener('change', (event) => {
@@ -19,5 +23,74 @@ title.addEventListener('change', (event) => {
     }
 
 });
+
+/***
+ * event listener for ...
+ */
+desing.addEventListener('change', (event) =>{
+    const selected = event.target.value;
+    const jspuns = document.querySelectorAll("#design option")[1];
+    const heartjs = document.querySelectorAll("#design option")[2];
+
+    const tshirtColors = document.querySelectorAll("#color option");
+    console.log("length " + tshirtColors.length);
+    for (let i = 0; i < tshirtColors.length; i++) {
+        tshirtColors[i].style.display = 'none';
+        if (selected == jspuns.value) {
+            jspuns.selected = true;
+            if (i >= 1 && i <= 3) {
+                tshirtColors[i].style.display = 'block';
+            }
+        } else if (selected == heartjs.value) {
+            heartjs.selected = true;
+            if (i >= 4 && i <= 6) {
+                tshirtColors[i].style.display = 'block';
+            }
+        }else{
+            //show "placeholder" for the color if neither design is selected
+            tshirtColors[0].style.display = 'block';
+        }
+    }
+});
+
+let costs = 0;
+const totalCosts = document.createElement("div");
+activities.appendChild(totalCosts);
+/***
+ *event listener for Activities: calculates total costs of the conference
+ */
+activities.addEventListener('change', (event) =>{
+    const checked = event.target.checked;
+    const inputs = document.querySelectorAll(".activities input");
+    const timeOfActivity = event.target.getAttribute('data-day-and-time');
+    const costOfActivity = parseInt(event.target.getAttribute('data-cost'));
+
+    for (let i = 0; i< inputs.length; i++){
+        const time = inputs[i].getAttribute('data-day-and-time');
+        console.log("time = " + time + ' timeOfActivity = ' + timeOfActivity);
+        if (time == timeOfActivity && event.target != inputs[i]){
+            //!!!
+            if(checked){
+                inputs[i].disabled = true;
+                //inputs[i].style.color = "#808080";
+            }else {
+                inputs[i].disabled = false;
+            }
+        }
+    }
+
+    if (checked){
+        costs += costOfActivity;
+    }else{
+        costs = costs - costOfActivity;
+    }
+    if(costs === 0){
+        totalCosts.style.display = "none";
+    } else {
+        totalCosts.style.display = "block";
+        totalCosts.innerText = `Your total costs are: $ ${costs}`;
+    }
+});
+
 
 
